21df834709d2e886eaf01a5be9d15a9c4b0494f2
gordo <rogelioalbertopedraza@gmail.com>
2022-07-18 18:47:57 -0500

    navbar component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 import Nav from "./components/Nav";
 
 function App() {
   const [tabs] = useState([
     {
       name: "about",
-      description: "a brief summary of me"
+      description: "a brief summary of me",
     },
     {
       name: "portfolio",
-      description: "a collection of some my projects"
+      description: "a collection of some my projects",
     },
     {
       name: "contact",
-      description: "get in touch with me here"
+      description: "get in touch with me here",
     },
     {
       name: "resume",
-      description: "download my resume for a full list of my skills"
-    }
-  ])
-  return (
-    <main>
+      description: "download my resume for a full list of my skills",
+    },
+  ]);
 
+  const [contactSelected, setContactSelected] = useState(false);
+
+  const [currentTab, setCurrentTab] = useState(tabs[0]);
+
+  return (
+    <div>
+      <Nav
+        tabs={tabs}
+        setCurrentTab={setCurrentTab}
+        currentTab={currentTab}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}>
+      </Nav>
+    <main>
     </main>
-  );
+    </div>
+  )
 }
 
 export default App;
---
+++ src/components/Nav/index.js
@@ -0,0 +1,59 @@
+import React, { useEffect } from "react";
+import { capitalizeFirstLetter } from "../../utils/helpers";
+import avatar from "../../assets/images/avatar.svg";
+
+function Nav(props) {
+  const {
+    tabs = [],
+    setCurrentTab,
+    currentTab,
+    contactSelected,
+    setContactSelected,
+  } = props;
+
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentTab.name);
+  }, [currentTab]);
+
+  return (
+    <header className="d-flex justify-content-between align-items-center mx-4 fw-bold">
+      <h2 className="text-light justify">
+        <img className="w-25" src={avatar} alt="rogelio avatar" />
+        Rogelio P!
+      </h2>
+      <nav>
+        <ul className="d-flex justify-content-between text-light">
+          <li className="mx-2">
+            <a
+              className="text-light no-text-deco is-clickable"
+              data-testid="about"
+              href="#about"
+              onClick={() => setContactSelected(false)}>
+              About
+            </a>
+          </li>
+          <li className={`mx-2 is-clickable ${contactSelected && "nav-active"}`}>
+            <span onClick={() => setContactSelected(true)}>Contact</span>
+          </li>
+          {tabs.map((tab) => (
+            <li
+              className={`mx-2 text-light ${
+                currentTab.name === tab.name && !contactSelected && `navActive`
+              }`}
+              key={tab.name}>
+              <span
+                onClick={() => {
+                  setCurrentTab(tab);
+                  setContactSelected(false);
+                }} className="is-clickable">
+                {capitalizeFirstLetter(tab.name)}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Nav;
