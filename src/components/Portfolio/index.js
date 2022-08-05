40ec9a939d801432fe17507afa3e697d354f6884
gordo <rogelioalbertopedraza@gmail.com>
2022-08-04 21:03:51 -0500

    added tweezer

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,36 +1,39 @@
 import React from 'react';
 import budgetTracker from "../../assets/images/projects/budget_tracker.png"
-import employeeTracker from "../../assets/images/projects/employee_tracker.png"
+import tweezer from "../../assets/images/projects/tweezerSS.png"
 import habitTracker from "../../assets/images/projects/habit_tracker.png"
 import moodswingr from "../../assets/images/projects/moodswingr.png"
 
 function Portfolio() {
-    const projects =[
-        {
-            name: "Budget Tracker",
-            description: "A budget tracker application that allows offline access and functionality. ",
-            link: "https://github.com/realnifty/budget-tracker",
-            image: budgetTracker
-        },
-        {
-            name: "Employee Tracker",
-            description: "A command-line application that manages a company's employee database, using Node.js, Inquirer, and MySQL.",
-            link: "https://github.com/realnifty/employee-tracker",
-            image: employeeTracker
-        },
-        {
-            name: "Habitual",
-            description: "An application that allows users to track their daily habits in a visually simplified display.",
-            link: "https://habitualtracker.herokuapp.com/",
-            image: habitTracker
-        },
-        {
-            name: "Moodswingr",
-            description: "An application that recommends songs based on your current mood.",
-            link: "https://realnifty.github.io/moodswingr/",
-            image: moodswingr
-        }
-    ]
+    const projects = [
+      {
+        name: "Budget Tracker",
+        description:
+          "A budget tracker application that allows offline access and functionality. ",
+        link: "https://github.com/realnifty/budget-tracker",
+        image: budgetTracker,
+      },
+      {
+        name: "Tweezer",
+        description: "This is a twitter-clone full-stack application.",
+        link: "https://tweezer-app.herokuapp.com/",
+        image: tweezer,
+      },
+      {
+        name: "Habitual",
+        description:
+          "An application that allows users to track their daily habits in a visually simplified display.",
+        link: "https://habitualtracker.herokuapp.com/",
+        image: habitTracker,
+      },
+      {
+        name: "Moodswingr",
+        description:
+          "An application that recommends songs based on your current mood.",
+        link: "https://realnifty.github.io/moodswingr/",
+        image: moodswingr,
+      },
+    ];
 
     return (
         <div id='portfolio' className="d-flex my-5">
